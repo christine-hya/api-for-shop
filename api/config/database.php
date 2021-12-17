031f27406894233a1147ce1fe23555e92b8ed865
christine-hya <81230662+christine-hya@users.noreply.github.com>
2021-12-17 19:38:19 +0200

    Database
    Change settings back to local and add database file.

--- api/config/database.php
+++ api/config/database.php
@@ -1,38 +1,10 @@
 <?php
 
-// class Database {
-//     private $host = "localhost";
-//     private $database_name = "webstore_portfolio";
-//     private $username = "root";
-//     private $password = "root";
-
-//     public $conn;
-
-//     public function getConnection(){
-//         $this->conn = null;
-//         try{
-//             $this->conn = new PDO (
-//                 "mysql:host=" .$this->host . ";
-//                 dbname=" . $this->database_name,
-//                 $this->username,
-//                 $this->password
-
-//             );
-//         }catch(PDOException $exception){
-//             echo "Database could not be connected: " .
-//             $exception->getMessage();
-
-//         }
-//         return $this->conn;
-//     }
-// }
-
 class Database {
-    private $host = "sql308.epizy.com";
-    // private $port = "3306";
-    private $database_name = "epiz_30614377_portfolioshop";
-    private $username = "epiz_30614377";
-    private $password = "fHdNAfTCsslYI";
+    private $host = "localhost";
+    private $database_name = "webstore_portfolio";
+    private $username = "root";
+    private $password = "root";
 
     public $conn;
 
@@ -54,3 +26,4 @@ class Database {
         return $this->conn;
     }
 }
+
