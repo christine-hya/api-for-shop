4f6d442ebb4bf07ec6a89b25696a0158d84b077f
christine-hya <81230662+christine-hya@users.noreply.github.com>
2021-12-17 16:56:09 +0200

    DB
    Update DB connection

--- api/config/database.php
+++ api/config/database.php
@@ -1,10 +1,38 @@
 <?php
 
+// class Database {
+//     private $host = "localhost";
+//     private $database_name = "webstore_portfolio";
+//     private $username = "root";
+//     private $password = "root";
+
+//     public $conn;
+
+//     public function getConnection(){
+//         $this->conn = null;
+//         try{
+//             $this->conn = new PDO (
+//                 "mysql:host=" .$this->host . ";
+//                 dbname=" . $this->database_name,
+//                 $this->username,
+//                 $this->password
+
+//             );
+//         }catch(PDOException $exception){
+//             echo "Database could not be connected: " .
+//             $exception->getMessage();
+
+//         }
+//         return $this->conn;
+//     }
+// }
+
 class Database {
-    private $host = "localhost";
-    private $database_name = "webstore_portfolio";
-    private $username = "root";
-    private $password = "root";
+    private $host = "sql308.epizy.com";
+    // private $port = "3306";
+    private $database_name = "epiz_30614377_portfolioshop";
+    private $username = "epiz_30614377";
+    private $password = "fHdNAfTCsslYI";
 
     public $conn;
 
